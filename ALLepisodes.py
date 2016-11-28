b53b8f3c35bc1f13b475d3652e0ec8cee19c7997
Ben Woodfield <raser.13@hotmail.com>
2016-11-28 14:15:07 +0000

    added some info (comments)

--- ALLepisodes.py
+++ ALLepisodes.py
@@ -1,11 +1,14 @@
+# A modification of Michael Kennedy's original script for scraping episodes
 # This version is aimed at the rss episodes link (all 80 podcasts)
+# All I changed is the url in line 11
+# Runs in Python 3 | Needs requests package | mp3s are 4.4GB in total
 
 import requests
 import sys
 from xml.etree import ElementTree
 
 def main():
-    rss_url = 'https://talkpython.fm/episodes/rss'
+    rss_url = 'https://talkpython.fm/episodes/rss' # Edited this link for all episodes
 
     resp = requests.get(rss_url)
     if resp.status_code != 200:
